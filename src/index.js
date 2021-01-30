d4fa7042bdb79807f729a4581dcad4d46c3420a8
oal2727 <joseluis_1555@hotmail.com>
2021-01-29 22:35:17 -0500

    five commit

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = process.env.PORT || 4000
 
+require("./database.js")
 
 
 app.use('/api',require("./controller/ControllerUser.js"))
@@ -25,15 +26,13 @@ app.use('/api',require("./controller/ControllerImage.js"))
 
 //
 // if(process.env.NODE_ENV === 'production'){
-app.use(express.static(path.join(__dirname,"../client/build")))
+app.use(express.static("../client/build")
 app.get('*',(req,res)=>{
-	res.sendFile(path.join(__dirname,'../client/build'));
+	res.sendFile(path.join(__dirname,'../client/build/index.html'));
 });
 // }
 
-require("./database.js")
 
-// console.log(path.join(__dirname,'../client/build'))
 // console.log(__dirname,"../client");
 // const patss = path.resolve(__dirname,"../client")
 app.listen(port,()=>{
