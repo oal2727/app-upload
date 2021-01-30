8854a5c74552f16ef4f53a706c381ac467ac835e
oal2727 <joseluis_1555@hotmail.com>
2021-01-29 22:43:45 -0500

    six commit

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.use('/api',require("./controller/ControllerImage.js"))
 
 //
 // if(process.env.NODE_ENV === 'production'){
-app.use(express.static("../client/build")
+app.use(express.static("../client/build"));
 app.get('*',(req,res)=>{
 	res.sendFile(path.join(__dirname,'../client/build/index.html'));
 });
