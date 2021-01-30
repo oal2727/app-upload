076f06c26bf4c0dab00c5b0088e6a1ebb1ee82d3
oal2727 <joseluis_1555@hotmail.com>
2021-01-30 10:32:43 -0500

    first commit

--- index.js
+++ index.js
@@ -18,19 +18,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = process.env.PORT || 4000
 
-require("./database.js")
+require("./src/database.js")
 
 
 app.use('/api',require("./controller/ControllerUser.js"))
 app.use('/api',require("./controller/ControllerImage.js"))
 
-//
-// if(process.env.NODE_ENV === 'production'){
-app.use(express.static("../client/build"));
-app.get('*',(req,res)=>{
-	res.sendFile(path.join(__dirname,'../client/build/index.html'));
-});
-// }
+
+
+
+if(process.env.NODE_ENV === 'production'){
+	app.use(express.static("./client/build"));
+// app.get('*',(req,res)=>{
+// 	res.sendFile(path.join(__dirname,'./client/build/index.html'));
+// });
+}
 
 
 // console.log(__dirname,"../client");
