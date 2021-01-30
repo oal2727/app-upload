98be7a86c6f0c0db899722923c02c2b246c170b1
oal2727 <joseluis_1555@hotmail.com>
2021-01-30 18:53:42 -0500

    eight deploy

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ app.use('/api',require("./src/controller/ControllerImage.js"))
 
 
 
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-}
+// }
 
 // app.use(express.static("client/build"));
 // app.get('*',(req,res)=>{
